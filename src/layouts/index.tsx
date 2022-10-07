f59312dd39c7332f7da46ed3fc84473e6a1a4ce5
Dengju Deng <wj871287@gmail.com>
2022-10-07 16:50:32 +0800

    fix: 更新左侧菜单树

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -8,50 +8,196 @@ export default function Layout({
   match,
 }: IRouteComponentProps) {
   return (
-    <div className="p-6 flex space-x-8">
-      <div className="w-64 space-y-4">
-        <div className="flex items-center pl-4">
-          <img
-            className="w-10 h-10 rounded-full"
-            src="https://avatars.githubusercontent.com/u/24560160?v=4"
-            alt="avatar"
-          />
-          <div className="pl-4">
-            <div className="text-xl">XRStudio</div>
-            <div className="text-xs">v0.0.1 Beta</div>
+    <div className="flex space-x-8">
+      <div className="w-64 h-screen p-6 flex flex-col justify-between">
+        <div className="space-y-4">
+          <div className="flex items-center pl-4">
+            <img
+              className="w-10 h-10 rounded-full"
+              src="https://avatars.githubusercontent.com/u/24560160?v=4"
+              alt="avatar"
+            />
+            <div className="pl-4">
+              <div className="text-xl">XRStudio</div>
+              <div className="text-xs">v0.0.1 Beta</div>
+            </div>
+          </div>
+
+          <div className="bg-white/5 rounded-xl h-64 p-4 space-y-2">
+            <Link
+              to="/"
+              className="flex items-center hover:text-white text-gray-400 bg-white/10 hover:bg-white/10 rounded-lg p-2 cursor-pointer"
+            >
+              <svg
+                className="w-5 h-5"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                />
+              </svg>
+              <div className="text-sm pl-2">工作台</div>
+            </Link>
+
+            <Link
+              to="/ar"
+              className="flex items-center hover:text-white text-gray-400 hover:bg-white/10 rounded-lg p-2 cursor-pointer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
+                />
+              </svg>
+              <div className="text-sm pl-2">AR 增强现实</div>
+            </Link>
+
+            <Link
+              to="/vr"
+              className="flex items-center hover:text-white text-gray-400 hover:bg-white/10 rounded-lg p-2 cursor-pointer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z"
+                />
+              </svg>
+              <div className="text-sm pl-2">VR 虚拟现实</div>
+            </Link>
+
+            <Link
+              to="/compile"
+              className="flex items-center hover:text-white text-gray-400 hover:bg-white/10 rounded-lg p-2 cursor-pointer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
+                />
+              </svg>
+              <div className="text-sm pl-2">特征点工具</div>
+            </Link>
+
+            <Link
+              to="/model"
+              className="flex items-center hover:text-white text-gray-400 hover:bg-white/10 rounded-lg p-2 cursor-pointer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
+                />
+              </svg>
+              <div className="text-sm pl-2">3D 模型库</div>
+            </Link>
           </div>
         </div>
 
-        <div className="bg-white/5 rounded-xl h-64 p-4 space-y-2">
-          <Link to="/" className="flex items-center text-white hover:text-white hover:bg-white/10 rounded-lg p-2 cursor-pointer">
+        <div className="rounded-xl p-4 space-y-2">
+          <Link
+            to="/archive"
+            className="flex items-center hover:text-white text-gray-400 hover:bg-white/10 rounded-lg p-2 cursor-pointer"
+          >
             <svg
-              className="w-5 h-5"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-5"
             >
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                strokeWidth={2}
-                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
               />
             </svg>
-            <div className="text-sm pl-2">工作台</div>
+            <div className="text-sm pl-2">废纸篓</div>
           </Link>
 
-          <Link to="/compile" className="flex items-center hover:text-white text-white hover:bg-white/10 rounded-lg p-2 cursor-pointer">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
+          <Link
+            to="/changelog"
+            className="flex items-center hover:text-white text-gray-400 hover:bg-white/10 rounded-lg p-2 cursor-pointer"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+              />
             </svg>
+            <div className="text-sm pl-2">更新日志</div>
+          </Link>
 
-            <div className="text-sm pl-2">特征点</div>
+          <Link
+            to="/helper"
+            className="flex items-center hover:text-white text-gray-400 hover:bg-white/10 rounded-lg p-2 cursor-pointer"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+              />
+            </svg>
+            <div className="text-sm pl-2">帮助 & 反馈</div>
           </Link>
         </div>
       </div>
 
-      <div className="flex-1">
+      <div className="flex-1 p-4">
         <div className="flex items-center py-5 space-x-4">
           <div className="text-sm">我的项目</div>
           <div className="py-1 px-2 text-xs bg-gray-500/50 rounded-full hover:bg-gray-500/75 cursor-pointer transition">
